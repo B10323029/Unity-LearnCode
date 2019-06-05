aef7d94682fae3b699d6ed58f81848a7ec2ea7f3
Pikabi.xw <34764506+B10323029@users.noreply.github.com>
2019-06-05 16:32:58 +0800

    練習2戰士新增，練習1練習建構函式

---
+++ Unity-LearnCode/Assets/練習2/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Soldier SoldierA = new Soldier();
+
+    public Warrior WarriorA = new Warrior();  
+
+    public void Start()
+    {
+        SoldierA.lv = 30;
+        Debug.Log("士兵經驗值 - 取得：" + SoldierA.GetExp());
+
+        SoldierA.SetExp(100);
+        Debug.Log("士兵經驗值 - 設定：" + SoldierA.exp);
+
+        WarriorA.lv = 50;
+        Debug.Log("戰士經驗值 - 取得：" + WarriorA.Exp);
+
+        WarriorA.Exp = 777;
+        Debug.Log("戰士經驗值 - 設定：" + WarriorA.Exp);
+    }
+
+    
+}
---
+++ Unity-LearnCode/Assets/練習2/Warrior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Warrior 
+{
+    public int exp;
+    public int lv;
+
+    /// <summary>
+    /// 取得與設定戰士經驗值
+    /// </summary>
+    public int Exp {
+        get
+        {
+            return lv * 10;
+        }
+        set
+        {
+            exp = value;
+        }
+    }
+
+}
