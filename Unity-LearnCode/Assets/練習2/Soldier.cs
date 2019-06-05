dafc3a69cdafcb6df0a626c60e8009e21a0b251e
Pikabi.xw <34764506+B10323029@users.noreply.github.com>
2019-06-05 15:01:36 +0800

    整理為練習1、2，練習2新增士兵

---
+++ Unity-LearnCode/Assets/練習2/Soldier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Soldier
+{
+    public int exp;
+    public int lv;
+
+    /// <summary>
+    /// 設定經驗值，計算經驗值為:等級乘上10 
+    /// </summary>
+    /// <returns>傳回計算後的經驗值</returns>
+    public int GetExp()
+    {
+        exp = lv * 10;
+        return exp;
+    }
+
+    /// <summary>
+    /// 設定經驗值
+    /// </summary>
+    /// <param name="getExp">取得經驗值</param>
+    public void SetExp(int getExp)
+    {
+        exp = getExp;
+    }
+}
