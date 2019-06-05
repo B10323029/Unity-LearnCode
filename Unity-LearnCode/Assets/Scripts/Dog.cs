263c57878663616445a9c8fdad7ce3f9f440f521
Pikabi.xw <34764506+B10323029@users.noreply.github.com>
2019-06-05 11:41:23 +0800

    新增狗狗類別欄位方法

---
+++ Unity-LearnCode/Assets/Scripts/Dog.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Dog
+{
+        
+    #region Dog 欄位設定
+    public string name;
+    public float weight;
+    public string ty
+pe;
+    public string sex;
+    public string color;
+    public int age;
+    #endregion
+
+    #region 方法
+    public void Bark()
+    {
+        Debug.Log(name + ":汪汪!!")
+    }
+    #endregion
+
+}
+
+public class DogAction
+{
+
+}
