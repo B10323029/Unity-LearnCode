07cc0fffd6c6f052d975c59e8c89cdb6cd8f0f6a
Pikabi.xw <34764506+B10323029@users.noreply.github.com>
2019-06-05 14:28:19 +0800

    新增程式及匯入Unity

--- Unity-LearnCode/Assets/Scripts/Dog.cs
+++ Unity-LearnCode/Assets/Scripts/Dog.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
 public class Dog
 {
         
     #region Dog 欄位設定
     public string name;
     public float weight;
-    public string ty
-pe;
+    public string type;
     public string sex;
     public string color;
     public int age;
@@ -19,7 +19,7 @@ pe;
     /// </summary>
     public void Bark()
     {
-        Debug.Log(name + ":汪汪!!")
+        Debug.Log(name + ":汪汪!!");
     }
 
     /// <summary>
@@ -31,13 +31,29 @@ pe;
         return age * 7;
     }
 
+    /// <summary>
+    /// 狗狗吃東西方法。
+    /// </summary>
+    /// <param name="food">食物</param>
+    /// <param name="speed">份量</param>
 
+    public void Eat(string food, string speed = "慢")
+    {
+        Debug.Log(name + ":" + food + "，速度：" + speed);
+    }
+
+    /// <summary>
+    /// 狗狗吃東西方法。
+    /// </summary>
+    /// <param name="food">食物</param>
+    /// <param name="speed">份量</param>
+
+    public void Eat(string food, int count)
+    {
+        Debug.Log(name + ":" + food + "，份量：" + count);
+    }
 
     #endregion
 
 }
 
-public class DogAction
-{
-
-}
---
+++ Unity-LearnCode/Assets/Scripts/DogManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+public class DogManager : MonoBehaviour
+{
+    public Dog DogA = new Dog();
+    public Dog DogB = new Dog();
+
+    
+
+    #region 事件 
+    private void Start()
+    {
+        DogA.name = "哈哈";
+        DogA.weight = 20.6f;
+        DogA.type = "哈士奇";
+        DogA.sex = "公";
+        DogA.color = "黑";
+        DogA.age = 2;
+
+        DogB.name = "柴柴";
+        DogB.weight = 15.8f;
+        DogB.type = "柴犬";
+        DogB.sex = "母";
+        DogB.color = "棕";
+        DogB.age = 3;
+
+        Debug.Log(DogA.name + "品種：" + DogA.type + "顏色：" + DogA.color);
+        Debug.Log(DogB.name + "品種：" + DogB.type + "顏色：" + DogB.color);
+
+        DogA.Bark();
+        DogB.Bark();
+
+        Debug.Log(DogA.name + "的實際年齡為：" + DogA.ConverAge());
+        Debug.Log(DogB.name + "的實際年齡為：" + DogB.ConverAge());
+
+        DogA.Eat("雞腿");
+        DogB.Eat("餅乾", 10);
+    }
+    #endregion
+}
